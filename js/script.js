d67243a029b619d9255c6020226c5bf21a7b3a1f
Vieyra <vieyraurenabravo@gmail.com>
2021-11-20 20:04:17 -0600

    added/home/header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+let options = document.querySelectorAll('.header__nav--links');
+let slide = document.querySelector('.slide');
 
+options.forEach((ele, index) => {
+  ele.addEventListener('mouseover', () => {
+    slide.style.left = 100 / options.length * index+'%'
+  });
+});
