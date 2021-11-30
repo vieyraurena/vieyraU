d8376f53352bbf8412554e63c6ebe340e1cf4238
Vieyra <vieyraurenabravo@gmail.com>
2021-11-29 19:59:39 -0600

    added/scrollUp/animationBubble/fixedSkills&footer

--- js/modules/nav.js
+++ js/modules/nav.js
@@ -1,9 +1,10 @@
 function nav() {
   const options = document.querySelectorAll('.header__nav--links');
-  const slide = document.querySelector('.slide');
 
+  const slide = document.querySelector('.slide');
+  
   options.forEach((ele, index) => {
-    ele.addEventListener('mouseover', () => {
+    ele.addEventListener('mouseover', (event) => {
       slide.style.left = 100 / options.length * index +'%';
     });
   });
